bd755f08830bca65848cf5a73e0ab993cabd2adc
bbashcode <binbash.code@gmail.com>
2022-02-20 06:26:48 +0000

    lotide - implemented countOnly

---
+++ countOnly.js
@@ -0,0 +1,53 @@
+/**
+ * assertEqual method is used to compare actual vs expected when testing
+ * @param  {any except array} actual The first param
+ * @param  {any except array} expected The second param
+ * @return {void/undefined}      this method does not return anything, rather the side effect is to console log test pass or fail based on comparison
+ */
+ const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  // inside the loop:
+  for(const item of allItems){
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
+
