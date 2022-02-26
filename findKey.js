078c70fbd413508bb868e09a64ed43dceab23281
bbashcode <binbash.code@gmail.com>
2022-02-26 07:18:53 +0000

    lotide - findkey method is implemented and working as expected

---
+++ findKey.js
@@ -0,0 +1,42 @@
+/**
+ * assertEqual method is used to compare actual vs expected when testing
+ * @param  {any except array} actual The first param
+ * @param  {any except array} expected The second param
+ * @return {void/undefined}      this method does not return anything, rather the side effect is to console log test pass or fail based on comparison
+ */
+ const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(objectParam, callback){
+  
+  for(let key in objectParam){
+    if(callback(objectParam[key])){
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// TEST CODE
+// console.log('result: ', findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2)); // => "noma")
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma');
