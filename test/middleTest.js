49411542ddc8c36759f8b0583e1a25e9c39d9b1d
bbashcode <binbash.code@gmail.com>
2022-02-27 06:32:26 +0000

    lotide refactor - middle.js: separated test files for middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+//TEST CODE for: middle.js
+// console.log(middle([1]));                     // => []
+// console.log(middle([1, 2]));                  // => []
+// console.log(middle([1, 2, 3]));               // => [2]
+// console.log(middle([1, 2, 3, 4, 5]));         // => [3]
+// console.log(middle([1, 2, 3, 4]));            // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6]));      // => [3, 4]
+
+const assertArraysEqual = require("../assertArraysEqual");
+const middle = require("../middle");
+
+assertArraysEqual([2, 3], middle([1, 2, 3, 4]));
