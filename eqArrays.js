f35cafca2a87fc65a15221fbd0cc224fbb560630
bbashcode <binbash.code@gmail.com>
2022-02-27 05:04:28 +0000

    lotide - eqArrays recursive cases are handled and everything working as expected

--- eqArrays.js
+++ eqArrays.js
@@ -20,16 +20,41 @@ const assertEqual = function(actual, expected) {
  * @param  {array} array2 The second param
  * @return {boolean} returns true or false depending on whether both arrays are equal or not
  */
- const eqArrays = (array1, array2) =>
- array1.length === array2.length &&
- array1.every((element, index) => element === array2[index]);
+//  const eqArrays = (array1, array2) =>
+//  array1.length === array2.length &&
+//  array1.every((element, index) => element === array2[index]);
+const eqArrays = function(array1, array2) {
+  let result = false;
+
+  if (array1.length === array2.length) {
+    array1.forEach((element, index) => {
+      if (Array.isArray(element)) {
+        result = eqArrays(array1[index], array2[index]);
+      } else {
+        if (element === array2[index]) {
+          result = true;
+        }
+      }
+    });
+  }
+  return result;
+};
+
+
+ 
 
 // TEST CODE
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true) // => false
-
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
-
-
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS (true)
+
+//RECURSIVE TEST CASES
+// console.log('should be true', eqArrays([[2, 3], [4]], [[2, 3], [4]]));
+// console.log('should be false', eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]));
+// console.log('should be false', eqArrays([[2, 3], [4]], [[2, 3], 4]));
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
