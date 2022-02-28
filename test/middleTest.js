ec13765cd47b0af04a27991da9b3b2eaae3e9f5f
bbashcode <binbash.code@gmail.com>
2022-02-28 05:38:09 +0000

    lotide testing - refactored middleTest to use mocha and chai for testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,8 @@
-//TEST CODE for: middle.js
-// console.log(middle([1]));                     // => []
-// console.log(middle([1, 2]));                  // => []
-// console.log(middle([1, 2, 3]));               // => [2]
-// console.log(middle([1, 2, 3, 4, 5]));         // => [3]
-// console.log(middle([1, 2, 3, 4]));            // => [2, 3]
-// console.log(middle([1, 2, 3, 4, 5, 6]));      // => [3, 4]
-
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
 const middle = require("../middle");
 
-assertArraysEqual([2, 3], middle([1, 2, 3, 4]));
+describe("testing middle function, it", ()=>{
+  it("should return [2, 3] for [1, 2, 3, 4]", ()=>{
+    assert.deepEqual(middle([1, 2, 3, 4]), [2,3]);
+  });
+});
